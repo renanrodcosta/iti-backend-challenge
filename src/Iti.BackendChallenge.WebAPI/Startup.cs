1787b7328d86aed2024ae362e38f67fd3e4c0156
Renan Costa <renan.rodcosta@gmail.com>
2020-03-05 16:30:14 -0300

    Removing using

--- src/Iti.BackendChallenge.WebAPI/Startup.cs
+++ src/Iti.BackendChallenge.WebAPI/Startup.cs
@@ -15,7 +15,6 @@ namespace Iti.BackendChallenge.WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -23,7 +22,6 @@ namespace Iti.BackendChallenge.WebAPI
             services.AddScoped<UseCases.ValidateStrongPassword>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
